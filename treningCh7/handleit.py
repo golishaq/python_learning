63963abfd17e90c3c2476c68bf7ef05e17f80933
Bartłomiej Goliszewski <golishaq@gmail.com>
2017-02-25 01:10:32 +0100

    text and binary files handling try/except

--- treningCh7/handleit.py
+++ treningCh7/handleit.py
@@ -1,1 +1,21 @@
 #demonstruje obsluge wyjatkow
+try:
+    num = float(input("Podaj liczbe: "))
+except ValueError:
+    print("wystapil blad, to nie była liczba")
+
+#kilka typow wyjatkow
+for value in (None, "hej", 10):
+    try:
+        print("Proba konwersji:",value,"-->", end=" ")
+        print(float(value))
+    except ValueError:
+        print("wystapil blad, to nie była liczba")
+    except TypeError:
+        print("wystapil blad, niewlasciwy typ danych")
+
+#wykorzystanie warunku wyjatku
+try:
+    num = float(input("Podaj liczbe: "))
+except ValueError as e:
+    print("wystapil blad, wywoales blad", e)

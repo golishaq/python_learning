7ad58e66dca8d8e2069eb1f128d0a422c8fa3742
Bartłomiej Goliszewski <golishaq@gmail.com>
2017-03-23 00:01:25 +0100

    OOP critter training

--- treningCh8/critter_keeper.py
+++ treningCh8/critter_keeper.py
@@ -2,7 +2,7 @@
 class Critter(object):
     """Wirtualny zwierzak
     Minimalny poziom hunger, boredom = 0"""
-    def __init__(self, name, hunger, = 0, boredom = 0):
+    def __init__(self, name, hunger = 0, boredom = 0):
         self.name = name
         self.hunger = hunger
         self.boredom = boredom
@@ -28,17 +28,46 @@ class Critter(object):
         print("nazywam sie", self.name, "i jestem", self.mood, "teraz \n")
         self.__pass_time()
     #zmniejsza poziom hunger o staly parametr
-    def eat(self, food = 4):
+    def eat(self, food = 3):
         print("Mniam, mniam. Dziekuje")
         self.hunger -= food
-        if self.hunger < 0
+        if self.hunger < 0:
             self.hunger = 0
         self.__pass_time()
     #zmniejsza poziom boredom o staly parametr
-    def play(self, fun =4):
+    def play(self, fun = 4):
         print("Hura")
         self.boredom -= fun
         if self.boredom <0:
             self.boredom =0
         self.__pass_time()
+def main():
+    crit_name = input("Jak chcesz nazwac swojego zwierzaka?")
+    crit = Critter(crit_name)
 
+    choice = None
+    while choice != "0":
+        print(
+        """Opiekun zwierzaka
+        0 - zakoncz
+        1 - sluchaj
+        2 - nakarm
+        3 - pobaw sie
+        """)
+        choice = input("wybierasz: ")
+
+        #obsługa menu
+        if choice == "0":
+            print("Do widzenia")
+        elif choice == "1":
+            crit.talk()
+        elif choice == "2":
+            food = int(input("Podaj ilosc jedzenia: "))
+            crit.eat(food)
+        elif choice == "3":
+            play = int(input("Jak dlugo chcesz sie bawic"))
+            crit.play(play)
+        else:
+            print("Niewlasiwa opcja \n")
+main()
+input("Press \"ENTER\" for exit")
